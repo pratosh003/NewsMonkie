fe2faa0913e0c37a7a08c25544a76e89e5037e51
pratosh003 <pratoshbm@gmail.com>
2021-10-08 12:02:11 +0530

    final

--- src/components/News.js
+++ src/components/News.js
@@ -7,14 +7,16 @@ import InfiniteScroll from 'react-infinite-scroll-component'
 
 const News = (props) => {
   const [articles, setArticles] = useState([]);
+  // eslint-disable-next-line
   const [loading, setLoading] = useState(true);
   const [page, setPage] = useState(1);
   const [totalResults, setTotalResults] = useState(0);
   //document.title = `"NewsMonkey - ${props.category}"`;
 
   const updateNews = async () => {
-    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=6a35325fdbb84d7bbeceae77d5ed430c&page=${page}&pageSize=${props.pageSize}`;
     setLoading(true)
+    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=6a35325fdbb84d7bbeceae77d5ed430c&page=${page}&pageSize=${props.pageSize}`;
+    
     let data = await fetch(url);
     let parsedData = await data.json();
 
@@ -45,6 +47,7 @@ const News = (props) => {
   return (
     <>
       <h2 className="text-center text-light " style={{ margin: '35px 0px', marginTop: '90px' }}>Top Headlines - {capitalizeFirstLetter(props.category)}</h2>     
+      {loading && <Spinner />}
       <InfiniteScroll
         dataLength={articles.length}
         next={fetchMoreData}
